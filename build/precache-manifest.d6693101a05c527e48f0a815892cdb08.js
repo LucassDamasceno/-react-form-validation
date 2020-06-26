37c85f7575d2c713efd0559fabf17c1de20cc59b
lucas damasceno <lucasdds13@gmail.com>
2020-06-26 16:52:31 -0300

    build

---
+++ build/precache-manifest.d6693101a05c527e48f0a815892cdb08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e144c1227f48feec398252b5b66935",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0eef4f48a6675ef36ef0",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a67a78513a21c35de93c",
+    "url": "/static/css/main.b0ac65ea.chunk.css"
+  },
+  {
+    "revision": "0eef4f48a6675ef36ef0",
+    "url": "/static/js/2.e3fa9945.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.e3fa9945.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a67a78513a21c35de93c",
+    "url": "/static/js/main.ce8e9ad9.chunk.js"
+  },
+  {
+    "revision": "bbaf42fbafa1bd8e01a4",
+    "url": "/static/js/runtime-main.26654654.js"
+  }
+]);
